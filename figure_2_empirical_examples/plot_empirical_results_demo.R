7bb51ee30cced9b9383756785df2545fc4ce504a
biozoo <clark.ms37@yahoo.com.tw>
2022-11-30 12:44:09 +0800

    Update plot_empirical_results_demo.R

--- figure_2_empirical_examples/plot_empirical_results_demo.R
+++ figure_2_empirical_examples/plot_empirical_results_demo.R
@@ -1,13 +1,13 @@
 ########################################################
 # Important Note:
-#  Dataset and documentation of all analytical procedures from a scientific paper, 
-#  "Anticipating the occurrence and type of critical transitions", that is now in revision pending with Science Advances.
+#  Datasets and documentation of all analytical procedures from a scientific paper, 
+#  Anticipating the occurrence and type of critical transitions, that will be formally published in Science Advances.
 #
 #   This R file demonstrates the DEV analysis applied in the following five datasets (Fig. 2A-O)
 #   (1) Microcosm experiment data provided by Dr. Egbert H. van Nes (Fig. 2A-C; microcosm.txt). 
 #       The complete dataset analyzed in the original paper (Veraart et al. 2012 Nature) has been published on online DANS repository, doi:10.17026/dans-ztg-93aw 
 #   (2) Voice experiment data are provided by Dr. Isao Tokuda (Fig. 2D-F; voice.txt) 
-#   (3) Cellular ATP experiment data are offered by Dr. Markus Schwarzlander (Fig. 2G-I; mitochondria.txt)
+#   (3) Cellular ATP experiment data are offered by Dr. Markus Schwarzländer (Fig. 2G-I; mitochondria.txt)
 #   (4) Greenhouse earth climate data is publicly available and were downloaded from the World Data Center for Paleoclimatology, 
 #       National Geophysical Data Center, Boulder, Colorado (http:// www.ncdc.noaa.gov/paleo/data.html). 
 #       This dataset has also been used for computing the other EWS in a previous study (Dakos et al. PNAS 2008)(Fig. 2J-L; climate.txt)
